2ef917b914177024cd035813b6f67d7dd60d9783
root <root@ubuntu.fpmislata.local>
2014-01-31 15:59:06 +0100

    first commit

--- showcachecpu.sh
+++ showcachecpu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cachecpu=$(cat /proc/cpuinfo | grep "cache size" | cut -d ":" -f2)
+cachecpu=$(cat /proc/cpuinfo | grep "cache size" | cut -d ":" -f2 | cut -d " " -f2)
 procesador=$(cat /proc/cpuinfo | grep "model name" | cut -d ":" -f2)
 fpu=$(cat /proc/cpuinfo | grep -P "^fpu\t" | cut -d ":" -f2)
-echo "El procesador $procesador tiene una cache de $cachecpu"
+echo "El procesador $procesador tiene una cache de $cachecpu KIB"
 if [ "$fpu" == " yes" ]; then
 echo si que tiene fpu
 fi
